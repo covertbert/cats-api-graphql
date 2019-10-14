33248b12b767fe1f4a4ce1c3c0fccebee7a43ba6
Bertie Blackman <blackmanrgh@gmail.com>
2019-10-14 10:46:56 +0100

    Refactors catservice test

--- src/services/__tests__/CatService.ts
+++ src/services/__tests__/CatService.ts
@@ -1,8 +1,8 @@
 import { Cat, CatFilter } from '../../types/Cat'
 import { filterCats } from '../CatService'
 
-const cats: Array<Partial<Cat>> = [
-  {
+const cats = {
+  goose: {
     name: 'Goose',
     adaptability: 5,
     hairless: false,
@@ -13,7 +13,7 @@ const cats: Array<Partial<Cat>> = [
       kg: [3, 5],
     },
   },
-  {
+  duck: {
     name: 'Duck',
     adaptability: 2,
     hairless: true,
@@ -24,46 +24,24 @@ const cats: Array<Partial<Cat>> = [
       kg: [1, 2],
     },
   },
-]
+}
+
+const catsArray: Array<Partial<Cat>> = Object.values(cats)
 
 describe('filterCats', () => {
   it('filters out hairless cats', () => {
     const filters: CatFilter[] = [{ key: 'hairless', value: false }]
 
-    const expectedResult = [
-      {
-        name: 'Goose',
-        adaptability: 5,
-        hairless: false,
-        lifeSpan: [14, 15],
-        temperament: ['Active', 'Energetic'],
-        weight: {
-          lbs: [7, 10],
-          kg: [3, 5],
-        },
-      },
-    ]
+    const expectedResult = [cats.goose]
 
-    expect(filterCats(cats as Cat[], filters)).toEqual(expectedResult)
+    expect(filterCats(catsArray as Cat[], filters)).toEqual(expectedResult)
   })
 
   it('filters out cats with an adaptability level that does not equal 2', () => {
     const filters: CatFilter[] = [{ key: 'adaptability', value: 2 }]
 
-    const expectedResult = [
-      {
-        name: 'Duck',
-        adaptability: 2,
-        hairless: true,
-        lifeSpan: [11, 12],
-        temperament: ['Floats', 'Flies'],
-        weight: {
-          lbs: [2, 4],
-          kg: [1, 2],
-        },
-      },
-    ]
+    const expectedResult = [cats.duck]
 
-    expect(filterCats(cats as Cat[], filters)).toEqual(expectedResult)
+    expect(filterCats(catsArray as Cat[], filters)).toEqual(expectedResult)
   })
 })
