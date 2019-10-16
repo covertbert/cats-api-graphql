ea4164ae2ff88581234af623d3bd6e030ac37d54
Bertie Blackman <blackmanrgh@gmail.com>
2019-10-16 13:56:03 +0100

    Removes the kill

--- docker/run.sh
+++ docker/run.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-# function on_exit {
-#     docker-compose stop
-#     docker-compose kill
-#     docker-compose rm -vf
-# }
 
-# trap on_exit EXIT
-
-# docker-compose kill
-# docker-compose rm -vf
-# docker-compose build
+docker-compose kill
+docker-compose rm -vf
+docker-compose build
 docker-compose up -d
