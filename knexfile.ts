731718a45cc18d70efd0c02ec1b76a07c96772ee
Bertie Blackman <blackmanrgh@gmail.com>
2019-10-30 13:44:45 +0000

    Knex

--- knexfile.ts
+++ knexfile.ts
@@ -1,8 +1,13 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: `postgres://root:password@${process.env.DB_HOST ||
-      'localhost'}:5432/catsdb`,
+    connection: {
+      database: 'catsdb',
+      // host: process.env.DB_HOST || 'localhost',
+      password: 'password',
+      socketPath: `/cloudsql/${process.env.APP_DB_HOST}`,
+      user: 'root',
+    },
     migrations: {
       directory: './db/migrations',
     },
