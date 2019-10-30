835e97cca1ffc5d2cd26d44c891550052bed0fc0
Bertie Blackman <blackmanrgh@gmail.com>
2019-10-30 15:55:30 +0000

    Fixes CI

--- knexfile.ts
+++ knexfile.ts
@@ -1,12 +1,8 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: {
-      database: 'catsdb',
-      host: process.env.DB_HOST || 'localhost',
-      password: 'password',
-      user: 'root',
-    },
+    connection: `postgres://root:password@${process.env.DB_HOST ||
+      'localhost'}:5432/catsdb`,
     migrations: {
       directory: './db/migrations',
     },
